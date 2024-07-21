676bc72472d9d84d4895acbbfbc4023976833cbb
Ratan Yadav <143277039+ratanyadav476@users.noreply.github.com>
2024-07-21 22:35:11 +0530

    Maximum Element in Array List

---
+++ ArrayList problems/Maximumelement.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Maximumelement {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+        System.out.println(list);
+
+        int max = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            if (max < list.get(i)) {
+                max = list.get(i);
+            }
+        }
+        System.out.println(max);
+    }
+}
