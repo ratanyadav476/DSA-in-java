432da3938d6cb1951a83c575d7096bcce1e2955a
Ratan Yadav <143277039+ratanyadav476@users.noreply.github.com>
2024-07-30 22:36:27 +0530

    LinkedList problem1

---
+++ LinKedList problem/linkesList2.java
@@ -0,0 +1,175 @@
+public class linkesList2 {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    // add firsth
+    public void addFirsth(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("ll is empity");
+            return;
+        }
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public void add(int idx, int data) {
+        if (idx == 0) {
+            addFirsth(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public int removeFirsth() {
+        if (size == 0) {
+            System.out.println("ll is empity");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    public int removeLast() {
+        if (size == 0) {
+            System.out.println("ll is null");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        Node prev = head;
+        for (int i = 0; i < size - 2; i++) {
+            prev = prev.next;
+        }
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+
+    }
+
+    public int iterSearch(int key) {
+        Node temp = head;
+        int i = 0;
+        while (temp != null) {
+            if (temp.data == key) {
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        return -1;
+    }
+
+    public void reverse() {
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    public void deletNthFromEnd(int n) {
+        if (n == size) {
+            head = head.next;
+            return;
+        }
+        int i = 1;
+        int iToFind = size - n;
+        Node prev = head;
+        while (i < iToFind) {
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+        return;
+    }
+
+    public static void main(String[] args) {
+        linkesList2 ll = new linkesList2();
+
+        ll.addFirsth(2);
+        ll.print();
+        ll.addFirsth(1);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
+
+        // ll.add(0, 9);
+        // ll.print();
+        // System.out.println(ll.size);
+        // ll.removeFirsth();
+        // ll.print();
+        // ll.removeLast();
+        // ll.print();
+        // System.out.println(ll.iterSearch(3));
+        // ll.reverse();
+        // ll.print();
+        ll.deletNthFromEnd(3);
+        ll.print();
+    }
+}
