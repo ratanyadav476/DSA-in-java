cf9a30f18d312241ad0c3dc0e778f5dd47a5ec41
Ratan Yadav <143277039+ratanyadav476@users.noreply.github.com>
2024-08-04 02:50:45 +0530

    Circular queue next

---
+++ QueueDataStructure/CircularQueueTwo.java
@@ -0,0 +1,71 @@
+public class CircularQueueTwo {
+    static class Queue {
+        static int arr[];
+        static int size;
+        static int rear = -1;
+        static int front = -1;
+
+        Queue(int size) {
+            arr = new int[size];
+            this.size = size;
+        }
+
+        // check if queue is empty or not
+        public static boolean isEmpty() {
+            return rear == -1 && front == -1;
+        }
+
+        // check if queue full or free
+
+        public static boolean isFull() {
+            return (rear + 1) % size == front;
+        }
+
+        // add data in queue
+        public static void add(int data) {
+            if (isFull()) {
+                System.out.println("queue is full");
+                return;
+            }
+            if (front == -1) {
+                front = 0;
+            }
+            rear = (rear + 1) % size;
+            arr[rear] = data;
+        }
+
+        public static int remove() {
+            if (isEmpty()) {
+                System.out.println("queue is empty");
+                return -1;
+            }
+            int result = arr[front];
+            if (rear == front) {
+                rear = front = -1;
+            } else {
+                front = (front + 1) % size;
+            }
+            return result;
+        }
+
+        public static int peek() {
+            if (isEmpty()) {
+                System.out.println("queue is empty");
+                return -1;
+            }
+            return arr[front];
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue(3);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
