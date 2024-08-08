021eb1e04bba23f526ad6e708ea4978093f65bba
Ratan Yadav <143277039+ratanyadav476@users.noreply.github.com>
2024-08-08 23:28:40 +0530

    add node in the middle of LinkedList

---
+++ LinKedList problem/addintheMid.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+public class addintheMid {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirsth(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("ll is empity");
+            return;
+        }
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public Node FindMid() {
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public Node addMiddle(Node head, int data) {
+        Node newNode = new Node(data);
+        int idx = size / 2;
+        Node temp = head;
+        int i = 0;
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        addintheMid ll = new addintheMid();
+        ll.addFirsth(1);
+        ll.addFirsth(2);
+        ll.addFirsth(3);
+        ll.addFirsth(4);
+        ll.addMiddle(head, 5);
+        ll.print();
+    }
+}
