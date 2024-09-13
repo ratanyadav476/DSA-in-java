293857cd11130880561edbc66faad8c4fe9fa88b
Ratan Yadav <143277039+ratanyadav476@users.noreply.github.com>
2024-09-13 21:22:38 +0530

    pattern 2

---
+++ Patterns/pattern2.java
@@ -0,0 +1,14 @@
+public class pattern2 {
+    public static void numberPerimid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        numberPerimid(5);
+    }
+}
