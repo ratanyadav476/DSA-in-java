67f8b352006ac5faecc1a4027a94e4b56ae2967e
Ratan Yadav <143277039+ratanyadav476@users.noreply.github.com>
2024-10-08 09:19:20 +0530

    zero one trangle

---
+++ Patterns/Zero_one_Trangle.java
@@ -0,0 +1,25 @@
+/**
+ * 1
+ * 0 1
+ * 1 0 1
+ * 0 1 0 1
+ * 1 0 1 0 1
+ */
+public class Zero_one_Trangle {
+    public static void zeroonetringle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1" + " ");
+                } else {
+                    System.out.print("0" + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        zeroonetringle(5);
+    }
+}
